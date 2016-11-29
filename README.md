de00a1773f89ee1842d79fbbe0733b5289a623b0
Calvin Kniffin <calkniffin@gmail.com>
2016-11-29 15:24:26 -0600

    Board made, prompting user for input

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ticTacToe
+# TicTacToe
+Simple tic tac toe game
+
+# To Start the Game
+`$ npm install`
+
+`$ node game/ticTacToe.js`
+
+# To Play
+
+Enter your position that you want to play your X or O
+
+  1  |  2  |  3
+  -------------
+  4  |  5  |  6
+  -------------
+  7  |  8  |  9
+  -------------
--- game/ticTacToe.js
+++ game/ticTacToe.js
@@ -9,6 +9,36 @@ let board = {
   6 : '',
   7 : '',
   8 : '',
-  9 : '' 
+  9 : ''
 }
-prompt.start()
+
+const player1 = 'X'
+const player2 = 'O'
+
+let winners = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+
+function printBoard () {
+  console.log( ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '' + '\n' +
+               '---------\n' +
+               ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '' + '\n' +
+               '---------\n' +
+               ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '')
+}
+
+function place (tic, position) {
+  board[position] += tic
+}
+
+// printBoard()
+function turn (player) {
+  prompt.start()
+  console.log("It's your turn " + player)
+  prompt.get(['position'], function(err, result) {
+    if(result.position == '') {
+      place(player, position)
+      printBoard()
+    }
+  })
+}
+
+turn(player1)
